bb665bc2d3dc8879d4b9cdd420f9c3e503031867
Andrew Allen <asallen67@gmail.com>
2019-06-28 16:24:35 -0400

    done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Adapter from '../Adapter';
+import Adapter from '../Adapter.js';
 import TVShowList from './TVShowList';
 import Nav from './Nav';
 import SelectedShowContainer from './SelectedShowContainer';
@@ -17,38 +17,38 @@ class App extends Component {
   }
 
   componentDidMount = () => {
-    Adapter.getShows().then(shows => this.setState({shows}))
+    Adapter.getShows()
+    .then(shows => this.setState({shows: shows}) )
   }
 
   componentDidUpdate = () => {
     window.scrollTo(0, 0)
   }
 
-  handleSearch (e){
+  handleSearch = (e)=>{
     this.setState({ searchTerm: e.target.value.toLowerCase() })
   }
 
-  handleFilter = (e) => {
-    e.target.value === "No Filter" ? this.setState({ filterRating:"" }) : this.setState({ filterRating: e.target.value})
+  handleFilter = (e)=>{
+    e.target.value === "No Filter" ? this.setState({ filterByRating: '' }) : this.setState({ filterByRating: e.target.value})
   }
 
   selectShow = (show) => {
     Adapter.getShowEpisodes(show.id)
-    .then((episodes) => this.setState({
+    .then(episodes => this.setState({
       selectedShow: show,
-      episodes
+      episodes: episodes
     }))
   }
 
   displayShows = () => {
-    if (this.state.filterByRating){
-      return this.state.shows.filter((s)=> {
-        return s.rating.average >= this.state.filterByRating
-      })
-    } else {
+    if(this.state.filterByRating)
+      return this.state.shows.filter(s => s.rating.average >= this.state.filterByRating )
+    else
       return this.state.shows
-    }
   }
+
+  printstate = (e)=> console.log(this.state)
 
   render (){
     return (
@@ -56,7 +56,7 @@ class App extends Component {
         <Nav handleFilter={this.handleFilter} handleSearch={this.handleSearch} searchTerm={this.state.searchTerm}/>
         <Grid celled>
           <Grid.Column width={5}>
-            {!!this.state.selectedShow ? <SelectedShowContainer selectedShow={this.state.selectedShow} allEpisodes={this.state.episodes}/> : <div/>}
+            {!!this.state.selectedShow ? <SelectedShowContainer selectedShow={this.state.selectedShow} episodes={this.state.episodes}/> : <div/>}
           </Grid.Column>
           <Grid.Column width={11}>
             <TVShowList shows={this.displayShows()} selectShow={this.selectShow} searchTerm={this.state.searchTerm}/>
